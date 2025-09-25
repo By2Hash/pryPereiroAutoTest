47cf00b697025eb31bc3caa0d734687117ffa2bf
By2Hash <by2git2@gmail.com>
2025-09-25 18:13:05 -0300

    Interfaz Listsa, Consultar el numero de turnos ya esta, falta mostrar la fecha mas antigua

--- pryPereiroAutoTest/frmAutoTest.Designer.cs
+++ pryPereiroAutoTest/frmAutoTest.Designer.cs
@@ -42,13 +42,13 @@
             lblCantDominio6Caracteres = new Label();
             lblAñoMasAntiguo = new Label();
             lblCantTurnos = new Label();
-            txtDominio6Caracteres = new TextBox();
-            txtAñoMasAntiguo = new TextBox();
-            txtCantidadTurnos = new TextBox();
             lblEstadisticas = new Label();
             btnRegistrar = new Button();
             btnSalir = new Button();
             btnConsultar = new Button();
+            lblCantTurno = new Label();
+            lblMasAntiguo = new Label();
+            lblCantDominioCon6Caracteres = new Label();
             grpResgistro.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)nudAñoFabricacion).BeginInit();
             grpConsulta.SuspendLayout();
@@ -67,7 +67,7 @@
             grpResgistro.Controls.Add(nudAñoFabricacion);
             grpResgistro.Location = new Point(12, 40);
             grpResgistro.Name = "grpResgistro";
-            grpResgistro.Size = new Size(538, 267);
+            grpResgistro.Size = new Size(538, 304);
             grpResgistro.TabIndex = 0;
             grpResgistro.TabStop = false;
             // 
@@ -111,8 +111,9 @@
             // 
             txtTitular.Location = new Point(197, 200);
             txtTitular.MaxLength = 30;
+            txtTitular.Multiline = true;
             txtTitular.Name = "txtTitular";
-            txtTitular.Size = new Size(314, 31);
+            txtTitular.Size = new Size(314, 84);
             txtTitular.TabIndex = 3;
             txtTitular.TextChanged += txtTitular_TextChanged;
             // 
@@ -124,6 +125,7 @@
             txtNroTurno.Size = new Size(83, 31);
             txtNroTurno.TabIndex = 2;
             txtNroTurno.TextChanged += txtNroTurno_TextChanged;
+            txtNroTurno.KeyPress += txtNroTurno_KeyPress;
             // 
             // txtDominio
             // 
@@ -138,7 +140,7 @@
             // 
             nudAñoFabricacion.Location = new Point(197, 145);
             nudAñoFabricacion.Maximum = new decimal(new int[] { 2025, 0, 0, 0 });
-            nudAñoFabricacion.Minimum = new decimal(new int[] { 1900, 0, 0, 0 });
+            nudAñoFabricacion.Minimum = new decimal(new int[] { 1950, 0, 0, 0 });
             nudAñoFabricacion.Name = "nudAñoFabricacion";
             nudAñoFabricacion.Size = new Size(99, 31);
             nudAñoFabricacion.TabIndex = 0;
@@ -157,13 +159,13 @@
             // grpConsulta
             // 
             grpConsulta.BackColor = SystemColors.ActiveBorder;
+            grpConsulta.Controls.Add(lblCantDominioCon6Caracteres);
+            grpConsulta.Controls.Add(lblMasAntiguo);
+            grpConsulta.Controls.Add(lblCantTurno);
             grpConsulta.Controls.Add(lblCantDominio6Caracteres);
             grpConsulta.Controls.Add(lblAñoMasAntiguo);
             grpConsulta.Controls.Add(lblCantTurnos);
-            grpConsulta.Controls.Add(txtDominio6Caracteres);
-            grpConsulta.Controls.Add(txtAñoMasAntiguo);
-            grpConsulta.Controls.Add(txtCantidadTurnos);
-            grpConsulta.Location = new Point(12, 338);
+            grpConsulta.Location = new Point(12, 375);
             grpConsulta.Name = "grpConsulta";
             grpConsulta.Size = new Size(549, 221);
             grpConsulta.TabIndex = 5;
@@ -172,7 +174,7 @@
             // lblCantDominio6Caracteres
             // 
             lblCantDominio6Caracteres.AutoSize = true;
-            lblCantDominio6Caracteres.Location = new Point(6, 143);
+            lblCantDominio6Caracteres.Location = new Point(6, 156);
             lblCantDominio6Caracteres.Name = "lblCantDominio6Caracteres";
             lblCantDominio6Caracteres.Size = new Size(322, 25);
             lblCantDominio6Caracteres.TabIndex = 5;
@@ -196,35 +198,10 @@
             lblCantTurnos.TabIndex = 3;
             lblCantTurnos.Text = "Cantidad de Turnos";
             // 
-            // txtDominio6Caracteres
-            // 
-            txtDominio6Caracteres.Location = new Point(383, 143);
-            txtDominio6Caracteres.MaxLength = 6;
-            txtDominio6Caracteres.Name = "txtDominio6Caracteres";
-            txtDominio6Caracteres.ReadOnly = true;
-            txtDominio6Caracteres.Size = new Size(84, 31);
-            txtDominio6Caracteres.TabIndex = 2;
-            // 
-            // txtAñoMasAntiguo
-            // 
-            txtAñoMasAntiguo.Location = new Point(383, 83);
-            txtAñoMasAntiguo.Name = "txtAñoMasAntiguo";
-            txtAñoMasAntiguo.ReadOnly = true;
-            txtAñoMasAntiguo.Size = new Size(84, 31);
-            txtAñoMasAntiguo.TabIndex = 1;
-            // 
-            // txtCantidadTurnos
-            // 
-            txtCantidadTurnos.Location = new Point(383, 27);
-            txtCantidadTurnos.Name = "txtCantidadTurnos";
-            txtCantidadTurnos.ReadOnly = true;
-            txtCantidadTurnos.Size = new Size(84, 31);
-            txtCantidadTurnos.TabIndex = 0;
-            // 
             // lblEstadisticas
             // 
             lblEstadisticas.AutoSize = true;
-            lblEstadisticas.Location = new Point(12, 310);
+            lblEstadisticas.Location = new Point(12, 347);
             lblEstadisticas.Name = "lblEstadisticas";
             lblEstadisticas.Size = new Size(102, 25);
             lblEstadisticas.TabIndex = 6;
@@ -253,12 +230,43 @@
             // 
             // btnConsultar
             // 
-            btnConsultar.Location = new Point(566, 338);
+            btnConsultar.Location = new Point(567, 375);
             btnConsultar.Name = "btnConsultar";
             btnConsultar.Size = new Size(112, 34);
             btnConsultar.TabIndex = 9;
             btnConsultar.Text = "&Consular";
             btnConsultar.UseVisualStyleBackColor = true;
+            btnConsultar.Click += btnConsultar_Click;
+            // 
+            // lblCantTurno
+            // 
+            lblCantTurno.AutoSize = true;
+            lblCantTurno.BackColor = SystemColors.Control;
+            lblCantTurno.Location = new Point(428, 30);
+            lblCantTurno.Name = "lblCantTurno";
+            lblCantTurno.Size = new Size(67, 25);
+            lblCantTurno.TabIndex = 6;
+            lblCantTurno.Text = "- - - - -";
+            // 
+            // lblMasAntiguo
+            // 
+            lblMasAntiguo.AutoSize = true;
+            lblMasAntiguo.BackColor = SystemColors.Control;
+            lblMasAntiguo.Location = new Point(428, 89);
+            lblMasAntiguo.Name = "lblMasAntiguo";
+            lblMasAntiguo.Size = new Size(72, 25);
+            lblMasAntiguo.TabIndex = 7;
+            lblMasAntiguo.Text = "- - - - - ";
+            // 
+            // lblCantDominioCon6Caracteres
+            // 
+            lblCantDominioCon6Caracteres.AutoSize = true;
+            lblCantDominioCon6Caracteres.BackColor = SystemColors.Control;
+            lblCantDominioCon6Caracteres.Location = new Point(428, 156);
+            lblCantDominioCon6Caracteres.Name = "lblCantDominioCon6Caracteres";
+            lblCantDominioCon6Caracteres.Size = new Size(72, 25);
+            lblCantDominioCon6Caracteres.TabIndex = 8;
+            lblCantDominioCon6Caracteres.Text = "- - - - - ";
             // 
             // frmAutoTest
             // 
@@ -267,7 +275,7 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.Control;
             CancelButton = btnSalir;
-            ClientSize = new Size(696, 576);
+            ClientSize = new Size(696, 671);
             ControlBox = false;
             Controls.Add(btnConsultar);
             Controls.Add(btnSalir);
@@ -280,6 +288,7 @@
             Name = "frmAutoTest";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Auto Test";
+            Load += frmAutoTest_Load;
             grpResgistro.ResumeLayout(false);
             grpResgistro.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)nudAñoFabricacion).EndInit();
@@ -304,13 +313,13 @@
         private GroupBox grpConsulta;
         private Label lblAñoMasAntiguo;
         private Label lblCantTurnos;
-        private TextBox txtDominio6Caracteres;
-        private TextBox txtAñoMasAntiguo;
-        private TextBox txtCantidadTurnos;
         private Label lblEstadisticas;
         private Label lblCantDominio6Caracteres;
         private Button btnRegistrar;
         private Button btnSalir;
         private Button btnConsultar;
+        private Label lblCantDominioCon6Caracteres;
+        private Label lblMasAntiguo;
+        private Label lblCantTurno;
     }
 }
---
+++ pryPereiroAutoTest/frmAutoTest.cs
@@ -0,0 +1,114 @@
+using System.Windows.Forms;
+
+namespace pryPereiroAutoTest
+{
+    public partial class frmAutoTest : Form
+    {
+        public frmAutoTest()
+        {
+            InitializeComponent();
+        }
+
+        int[] vecTurno = new int[50];
+
+
+
+        private void nudAñoFabricacion_ValueChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        int Indice = 0;
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            if (Indice < vecTurno.Length)
+            {
+                vecTurno[Indice] = Convert.ToInt32(txtNroTurno.Text);
+                Indice++;
+                txtNroTurno.Text = "";
+                txtDominio.Text = "";
+                nudAñoFabricacion.Value = 2000;
+                txtTitular.Text = "";
+
+            }
+
+
+
+        }
+
+        private void txtTitular_TextChanged(object sender, EventArgs e)
+        {
+            if (txtTitular.Text != "")
+            {
+                btnRegistrar.Enabled = true;
+
+            }
+            else
+            {
+                btnRegistrar.Enabled = false;
+            }
+        }
+
+        private void txtDominio_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNroTurno_TextChanged(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtNroTurno_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtNroTurno_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmAutoTest_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCantidadTurnos_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void grpConsulta_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            //En la etiqueta muestra el indice, no hace falta poner vecTurno[Indice]
+            lblCantTurno.Text = Indice.ToString();
+
+            //Año mas antiguo
+
+            // mostrar el resultado
+
+         
+
+
+        }
+    }
+
+}
